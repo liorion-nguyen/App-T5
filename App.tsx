261c109098935906198afb1b12119cf75fb39b50
LiorionNguyen <liorion.nguyen@gmail.com>
2025-01-16 19:00:42 +0700

    update

--- App.tsx
+++ App.tsx
@@ -7,24 +7,27 @@ import {
   Image,
   TouchableOpacity,
   Touchable,
+  ScrollView,
+  SafeAreaView,
 } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.layoutRow}>
-        <Text>Item 1</Text>
-        <Text>Item 2</Text>
-        <Text>Item 3</Text>
-        <Text>Item 4</Text>
-      </View>
-      <View style={styles.layoutColumn}>
-        <Text>Item 1</Text>
-        <Text>Item 2</Text>
-        <Text>Item 3</Text>
-        <Text>Item 4</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.containerView}>
+        <View style={styles.containerView1}>
+          <Text style={styles.title}>Login</Text>
+          <TextInput placeholder="Enter your name?" style={styles.input}/>
+          <TextInput placeholder="Enter your name?"/>
+          <TextInput placeholder="Enter your name?"/>
+          <TouchableOpacity >
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerView2}></View>
+        <View style={styles.containerView3}></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -34,20 +37,37 @@ const styles = StyleSheet.create({
     justifyContent: "center", // Căn chỉnh các thành con theo trục y
     alignItems: "center", // // Căn chỉnh các thành con theo trục x
   },
-  layoutRow: {
-    backgroundColor: "red",
-    flex: 1,
-    width: '100%',
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
+  containerView: {
+    width: "100%",
+    height: 100,
   },
-  layoutColumn: {
-    backgroundColor: "blue",
-    flex: 1,
-    width: '100%',
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
+  title: {
+    fontSize: 40, // Kích cỡ chữ
+    fontWeight: 600, // Kiểu in đậm
+    color: "white", // Màu chữ
+    textAlign: "center" // Căn chỉnh text theo chiều ngang
+  },
+  input: {
+    backgroundColor: "white", // Màu sắc phông nền
+    width: "80%", // Chiều rộng thành phần
+    padding: 10, // Tạo khoảng cách giữa content và border
+    borderRadius: 10, // Bo 4 góc
+    borderWidth: 1, // Chiều rộng đường viền
+    borderColor: "black" // Màu đường viền
+  },
+  containerView1: {
+    backgroundColor: "red",
+    width: "100%",
+    height: 500
+  },
+  containerView2: {
+    backgroundColor: "green",
+    width: "100%",
+    height: 500
+  },
+  containerView3: {
+    backgroundColor: "pink",
+    width: "100%",
+    height: 500
   }
 })
