52c7a9405b6e6d32e982b1e73a89d6e0b5ee2d51
LiorionNguyen <liorion.nguyen@gmail.com>
2025-02-13 23:04:03 +0700

    update

--- App.tsx
+++ App.tsx
@@ -17,18 +17,63 @@ import {
   Touchable,
   ScrollView,
   SafeAreaView,
+  LogBox,
 } from "react-native";
 
 export default function App() {
+  // code javacript
+  const name = "Nguyen Van A";
+  let age = 20;
+  const isMale = true;
+  const email = "a@gmail.com";
+  // Tạo biến để lưu trữ thông tin của bản thân: tên, tuổi, giới tính, email.
+
+  console.log("hello");
+  console.log(name);
+  console.log(1 + 2);
+  console.log(1 - (2 /2));
+  console.log(1 * 2);
+  console.log(1 / 2);
+  console.log(5 >= 3);
+
+  // and: &&
+  // or: ||
+  // not: !
+  console.log(true || true);
+  console.log(false || true);
+  console.log(false || false);
+  console.log(true && true);
+  console.log(true && false);
+  console.log(!true);
+  
+  console.log(Math.PI);
+  console.log(Math.max(3, 12, 6, 9));
+  console.log(Math.sqrt(16));
+  console.log(Math.pow(2, 3));
+  console.log(Math.round(Math.random() * 100)); // Math.random chỉ tạo ra từ 0 - 1
+
+  const web = "Kite";
+  const title = "Hello ," + name + "da den ";
+  const title2 = `Hello ${name} da den ${web}`;
+  console.log(title2);
+  
+  
+
+
+  
+
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.containerView}>
         <View style={styles.containerView1}>
           <Text style={styles.title}>Login</Text>
-          <TextInput placeholder="Enter your name?" style={styles.input}/>
-          <TextInput placeholder="Enter your name?"/>
-          <TextInput placeholder="Enter your name?"/>
-          <TouchableOpacity >
+          <View style={styles.groupInput}>
+            <TextInput placeholder="Enter your name?" style={styles.input} />
+            <TextInput placeholder="Enter your name?" style={styles.input} />
+            <TextInput placeholder="Enter your name?" style={styles.input} />
+          </View>
+          <TouchableOpacity style={styles.buttonTest}>
             <Text>Login</Text>
           </TouchableOpacity>
         </View>
@@ -45,18 +90,37 @@ const styles = StyleSheet.create({
     justifyContent: "center", // Căn chỉnh các thành con theo trục y
     alignItems: "center", // // Căn chỉnh các thành con theo trục x
   },
-  containerView: {
-    width: "100%",
+  box: {
+    width: 500,
     height: 100,
+    backgroundColor: "red",
+    margin: 10
+  },
+  containerView: {
+    width: "100%", // Chiều rộng thành phần
+    height: 100, // Chiều cao thành phần
+  },
+  groupInput: {
+    flexDirection: "row", // Xếp các thành phần theo chiều dọc
+    alignItems: "center", // Căn chỉnh các thành phần theo trục x
+    justifyContent: "space-between", // Căn chỉnh các thành phần theo trục y
+    width: "100%", // Chiều rộng thành phần
+    backgroundColor: "blue", // Màu nền
+    height: 300
   },
   title: {
     fontSize: 40, // Kích cỡ chữ
-    fontWeight: 600, // Kiểu in đậm
+    fontWeight: "bold", // Kiểu in đậm
     color: "white", // Màu chữ
-    textAlign: "center" // Căn chỉnh text theo chiều ngang
+    textAlign: "center", // Căn chỉnh text theo chiều ngang
+    backgroundColor: "black", // Màu nền
+    borderWidth: 5, // Chiều rộng đường viền
+    borderColor: "yellow", // Màu đường viền
+    padding: 50, // Tạo khoảng cách giữa content và border
+    marginBottom: 20 // Khoảng cách giữa các thành phần
   },
   input: {
-    backgroundColor: "white", // Màu sắc phông nền
+    backgroundColor: "#33CC66", // Màu sắc phông nền
     width: "80%", // Chiều rộng thành phần
     padding: 10, // Tạo khoảng cách giữa content và border
     borderRadius: 10, // Bo 4 góc
@@ -64,6 +128,7 @@ const styles = StyleSheet.create({
     borderColor: "black" // Màu đường viền
   },
   containerView1: {
+    flexDirection: "column",
     backgroundColor: "red",
     width: "100%",
     height: 500
@@ -77,5 +142,13 @@ const styles = StyleSheet.create({
     backgroundColor: "pink",
     width: "100%",
     height: 500
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  buttonTest: {
+    backgroundColor: "blue",
+    padding: 10
   }
 })
