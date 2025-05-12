d778afcf4522ea806036a98c84b6a0ee6a461b7b
Liorion Nguyen <liorion.nguyen@gmail.com>
2025-05-12 21:22:53 +0700

    update

--- src/components/ReceiveData.tsx
+++ src/components/ReceiveData.tsx
@@ -9,25 +9,10 @@ export default function ReceiveData() {
         if (user) {
             fetch("https://l-edu.koyeb.app/users", {
                 "headers": {
-                  "accept": "application/json, text/plain, */*",
-                  "accept-language": "en-US,en;q=0.9,vi;q=0.8",
                   "authorization": `Bearer ${user.data.access_token}`,
-                  "cache-control": "no-cache",
-                  "pragma": "no-cache",
-                  "priority": "u=1, i",
-                  "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
-                  "sec-ch-ua-mobile": "?0",
-                  "sec-ch-ua-platform": "\"macOS\"",
-                  "sec-fetch-dest": "empty",
-                  "sec-fetch-mode": "cors",
-                  "sec-fetch-site": "cross-site"
                 },
                 "referrer": "https://l-edu.vercel.app/",
-                "referrerPolicy": "strict-origin-when-cross-origin",
-                "body": null,
-                "method": "GET",
-                "mode": "cors",
-                "credentials": "include"
+                "method": "GET"
               }).then(res => res.json()).then(data => {
                 setData(data.data);
               });
